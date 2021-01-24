f3367eb4def4390310ec5c188abe4e8be6153196
神田成樹 <64734524+kanda16g@users.noreply.github.com>
2021-01-24 22:06:03 +0900

    Initial commit

---
+++ houou_js_make/js/script.js
@@ -0,0 +1,16 @@
+$(function() {
+
+    $(window).on('scroll', function() {
+
+        //ころりん
+        if($(window).scrollTop() > 300) {
+            $('.circle-banner').addClass('is-over');
+        } else {
+            $('.circle-banner').removeClass('is-over');
+        }
+        if($(window).scrollTop() > $('.footer').offset().top - 1000) {
+            $('.circle-banner').removeClass('is-over');
+        }
+    });
+
+});
